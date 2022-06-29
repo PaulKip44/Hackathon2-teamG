83a84b9538dfc459b95edba3425b91666697059b
Marc LANTOL <marc.lantol@gmail.com>
2022-06-29 16:30:06 +0200

    Project description component done

---
+++ frontend/src/components/Description/Description.jsx
@@ -0,0 +1,15 @@
+import "./Description.css";
+
+export default function Description() {
+  return (
+    <section id="project-description">
+      <h1>Description du projet</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        maiores praesentium nostrum fuga corporis voluptatem, veritatis eius
+        commodi, vel distinctio labore cumque totam saepe eaque. Alias ut
+        explicabo inventore quasi.
+      </p>
+    </section>
+  );
+}
