cd0821e9516e6deb20dcef8401f11f9a14f6b228
Valerie <valerie.ielsch@gmail.com>
2022-06-29 14:55:45 +0200

    root css and logo added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,27 @@
-import Counter from "@components/Counter";
-import logo from "@assets/logo.svg";
+import logo from "@assets/logo-apside.png";
 
 export default function Home() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
+      <img src={logo} width="15%" alt="apside logo" />
+      <h1>Hello Wilder WELCOME</h1>
+      <h2>Hello Wilder WELCOME</h2>
+      <h3>Hello Wilder WELCOME</h3>
+      <p>Hello Wilder WELCOME</p>
+      <a
+        href="https://github.com/PaulKip44/Hackathon2-teamG/tree/rootCss"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Link Github
+      </a>
+      <br />
+      <button type="button" className="btn-blue">
+        Button1
+      </button>
+      <button type="button" className="btn-orange">
+        Button2
+      </button>
     </header>
   );
 }
