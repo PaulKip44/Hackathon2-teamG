b7d1bfbe3fa35c6a4b0871eca91dfa05249c4706
Marc LANTOL <marc.lantol@gmail.com>
2022-06-29 18:01:26 +0200

    Create routes for login and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import Dashboard from "./pages/Dashboard";
-// import Home from "@pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "@pages/Dashboard";
+import Login from "@pages/Login";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
