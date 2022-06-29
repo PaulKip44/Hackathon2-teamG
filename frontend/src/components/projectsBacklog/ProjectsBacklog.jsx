c1ef51d6f526328e1d8835e9d04fad60c4e3f914
Basile Carle <puma@iMac-de-Basile.local>
2022-06-29 23:50:19 +0200

    added the possiblity to select a user through a pull menu waiting for the capability to know which user has loggued

--- frontend/src/components/projectsBacklog/ProjectsBacklog.jsx
+++ frontend/src/components/projectsBacklog/ProjectsBacklog.jsx
@@ -5,17 +5,41 @@ import api from "@services/endpoint";
 
 const ProjectsBacklog = () => {
   const [user, setUser] = useState(1);
+  const [users, setUsers] = useState([]);
   const [projects, setProjects] = useState([]);
-  const ENDPOINT = `/projects`;
+  const ENDPOINTPROJECTS = `/projects`;
+  const ENDPOINTUSERS = `/users`;
   useEffect(() => {
-    api.get(ENDPOINT).then((result) => {
+    api.get(ENDPOINTPROJECTS).then((result) => {
       console.error(result.data[0]);
       setProjects(result.data);
+    });
+    api.get(ENDPOINTUSERS).then((result) => {
+      console.error(result.data[0]);
+      setUsers(result.data);
     });
   }, []);
 
   return (
     <>
+      <div className="user-selection">
+        <select
+          name="user"
+          id="user-select"
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+        >
+          <option value="">-- Please select a user</option>
+          {users.map((user) => {
+            return (
+              <option value={user.Id}>
+                {user.firstname} {user.lastname}
+              </option>
+            );
+          })}
+        </select>
+      </div>
       {projects.map((project) => {
         return (
           <DisplayProject project={project} key={project.Id} user={user} />
