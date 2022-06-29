76d3ef63177c9aacf30a975bc311c22158ff4dbc
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-06-29 17:30:47 +0200

    add create, delete, findAll, findOne

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,4 +11,11 @@ router.get("/users/:id", UserController.read);
 router.post("/users", UserController.add);
 router.delete("/users/:id", UserController.delete);
 
+// ROUTES PROJECT
+
+router.get("/projects", ProjectController.browse);
+router.get("/projects/:id", ProjectController.read);
+router.post("/projects", ProjectController.add);
+router.delete("/projects/:id", PrpjectController.delete);
+
 module.exports = router;
