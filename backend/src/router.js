03de47c3d55faa5c3d526e08327372d10f7ade14
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-06-29 17:06:00 +0200

    add create, delete, findAll, findOne

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const { UserController } = require("./controllers");
 
 const router = express.Router();
 
-router.get("/items", ItemController.browse);
-router.get("/items/:id", ItemController.read);
-router.put("/items/:id", ItemController.edit);
-router.post("/items", ItemController.add);
-router.delete("/items/:id", ItemController.delete);
+// ROUTES USER
+
+router.get("/users", UserController.browse);
+router.get("/users/:id", UserController.read);
+router.post("/users", UserController.add);
+router.delete("/users/:id", UserController.delete);
 
 module.exports = router;
