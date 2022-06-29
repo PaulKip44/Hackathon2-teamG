483f89b47fa5e9be09164c212f4ef0aee050a410
Marie_Tignol <marie-tignol_student2022@wilder.school>
2022-06-29 17:39:30 +0200

    create login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "@pages/Home";
+import Login from "./pages/Login";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
