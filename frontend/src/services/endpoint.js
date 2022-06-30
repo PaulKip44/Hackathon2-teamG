4197be9f21ec44dab1fd6ba329077dae7e86a49a
val0sch <88197337+val0sch@users.noreply.github.com>
2022-06-30 08:28:23 +0200

    Update endpoint.js

--- frontend/src/services/endpoint.js
+++ frontend/src/services/endpoint.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
+const endpoint = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
-export default api;
+export default endpoint;
