efe6ba4b2168379327c46caa628fec28bd1d0938
Valerie <valerie.ielsch@gmail.com>
2022-07-01 10:10:21 +0200

    add fake map example for future improvment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "@pages/Dashboard";
 import Login from "@pages/Login";
 import NewsFeed from "@pages/NewsFeed";
 import "./App.css";
+import Map from "@components/Map";
 import { CurrentUserContextProvider } from "./context/userContext";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/dashboard/:projectId" element={<Dashboard />} />
             <Route path="/dashboard" element={<ProjectsBacklog />} />
             <Route path="/newsfeed" element={<NewsFeed />} />
+            <Route path="/map" element={<Map />} />
           </Routes>
         </BrowserRouter>
       </div>
